aca8af973fdc4559e8f183c4c13d622be0d0c086
NinjonikSVK <34453649+Ninjonik@users.noreply.github.com>
2024-03-29 22:13:01 +0100

    - initial stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,13 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "main-background": "https://static.vecteezy.com/system/resources/previews/021/739/427/original/simple-and-cool-3d-abstract-background-design-suitable-for-pc-backgrounds-and-others-vector.jpg",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
 export default config;
