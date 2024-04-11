7c1ecb2880715a9aaa543a03335dee72362da762
NinjonikSVK <34453649+Ninjonik@users.noreply.github.com>
2024-04-11 19:26:31 +0200

    - changed build port to 3009 - bug fixes - added new services - added built in search

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.weatherapi.com'],
+        domains: ['cdn.weatherapi.com', 'cdn.icon-icons.com', 'static-00.iconduck.com', 'seeklogo.com'],
     },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import {CurrentTime} from "@/app/components/CurrentTime";
 import {TemperatureGraph} from "@/app/components/TemperatureGraph";
 import SystemStats from "@/app/components/SystemStats";
+import {redirect} from "next/navigation";
 
 interface Params {
     [key: string]: string;
@@ -167,10 +168,22 @@ export default async function Home() {
         nameDay = nameDayData.nameday[country.toLowerCase()] || "";
     }
 
+    const handleForm = async (FormData: any) => {
+        "use server"
+
+        const query = FormData.get('query');
+        console.log(query)
+        redirect(
+            "https://search.igportals.eu/searxng?q=" + query,
+        )
+
+    }
+
     return (
         <main className="flex h-screen w-screen p-24 flex-col items-center justify-between bg-cover bg-no-repeat bg-opacity-75 bg-[url('https://static.vecteezy.com/system/resources/previews/021/739/427/original/simple-and-cool-3d-abstract-background-design-suitable-for-pc-backgrounds-and-others-vector.jpg')]">
             <div className="grid grid-rows-3 grid-cols-5 w-full h-full gap-4">
-                <div className="row-span-1 col-span-1 bg-base-100 p-8 rounded-lg">
+                <div className="row-span-1 col-span-1 bg-base-100 p-8 rounded-lg justify-center items-center flex flex-col">
+                    <Image src={'/iglogo.png'} sizes={'256px'} height={0} width={0} alt={'IGPortals Logo'} className={'h-3/4 w-3/4'}/>
                     <h1>Welcome to the IGPortals Dashboard.</h1>
                 </div>
 
@@ -188,7 +201,7 @@ export default async function Home() {
 
                             return (
                                 <div key={index}
-                                    className="row-span-1 col-span-1 bg-base-100 rounded-lg flex flex-col justify-center items-center text-center p-2">
+                                     className="row-span-1 col-span-1 bg-base-100 rounded-lg flex flex-col justify-center items-center text-center p-2">
                                     <span>{dayOfWeek}</span>
                                     <Image alt={day.day.condition.text}
                                            src={"https:" + day.day.condition.icon}
@@ -238,7 +251,57 @@ export default async function Home() {
                     <h2>Temperature Today</h2>
                     <TemperatureGraph forecastData={forecastData}/>
                 </div>
-                <SystemStats />
+                <SystemStats/>
+                <div className="row-span-1 col-span-1 grid gap-4 grid-cols-2 grid-rows-2">
+                    <a className='row-span-1 col-span-1 rounded-lg flex justify-center items-center text-center'
+                       href={'https://cloud.igportals.eu'} title={'Nextcloud'} target={"_blank"}>
+                        <div className="flex flex-col justify-between h-full items-center">
+                            <Image src={'https://cdn.icon-icons.com/icons2/2108/PNG/512/nextcloud_icon_130873.png'}
+                                   sizes={'512px'} height={0} width={0}
+                                   className='hover:opacity-75 transition-all ease-in h-full w-full' alt={'Nextcloud'}/>
+                        </div>
+                    </a>
+                    <a className='row-span-1 col-span-1 rounded-lg flex justify-center items-center text-center'
+                       href={'https://status.igportals.eu/status/hoi4intel'} title={'Status'} target={"_blank"}>
+                        <div className="flex flex-col justify-between h-full items-center">
+                            <Image src={'https://static-00.iconduck.com/assets.00/uptime-kuma-icon-512x469-ce3ut52z.png'}
+                                   sizes={'512px'} height={0} width={0}
+                                   className='hover:opacity-75 transition-all ease-in h-full w-full' alt={'Status'}/>
+                        </div>
+                    </a>
+                    <a className='row-span-1 col-span-1 rounded-lg flex justify-center items-center text-center'
+                       href={'https://ptero.igportals.eu'} title={'Pterodactyl'} target={"_blank"}>
+                        <div className="flex flex-col justify-between h-full items-center">
+                            <Image src={'https://cdn.icon-icons.com/icons2/1147/PNG/512/1486486315-archive-archives-files-hosting-database-server-storage_81222.png'}
+                                   sizes={'512px'} height={0} width={0}
+                                   className='hover:opacity-75 transition-all ease-in h-full w-full' alt={'Pterodactyl'}/>
+                        </div>
+                    </a>
+                    <a className='row-span-1 col-span-1 rounded-lg flex justify-center items-center text-center'
+                       href={'https://appwrite.igportals.eu'} title={'Appwrite'} target={"_blank"}>
+                        <div className="flex flex-col justify-between h-full items-center">
+                            <Image src={'https://seeklogo.com/images/A/appwrite-logo-D33B39992A-seeklogo.com.png'}
+                                   sizes={'512px'} height={0} width={0}
+                                   className='hover:opacity-50 transition-all ease-in h-full w-full' alt={'Appwrite'}/>
+                        </div>
+                    </a>
+                </div>
+                <div
+                    className='row-span-1 col-span-2 bg-base-100 rounded-lg flex flex-col gap-2 justify-evenly items-center text-center p-4'>
+                    <h2 className='text-2xl'>Search</h2>
+                    <form className='flex flex-row justify-evenly items-center w-full gap-4' action={handleForm}>
+                        <label className="input input-bordered flex items-center gap-2 w-full">
+                            <input type="text" className="grow" placeholder="Search" autoFocus={true} name={'query'}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                 className="w-4 h-4 opacity-70">
+                                <path fillRule="evenodd"
+                                      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        </label>
+                        <button className="btn btn-primary" type={"submit"} name={'submit'}>Search</button>
+                    </form>
+                </div>
             </div>
         </main>
     );
